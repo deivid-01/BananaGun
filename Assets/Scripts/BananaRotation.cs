50498a50dacb2c7f3655808d5ecc93b80f1dd621
deivid-01 <datcx@hotmail.com>
2021-04-10 18:13:43 -0500

    Enemy Spawner

---
+++ Assets/Scripts/BananaRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed*Time.deltaTime);
+    }
+}
