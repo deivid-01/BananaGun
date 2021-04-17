d35352763d7b2e4c26818d1598f9be8312d11c78
deivid-01 <datcx@hotmail.com>
2021-04-17 12:44:14 -0500

    Adding documentation

--- Assets/Scripts/ImageProcessing.cs
+++ Assets/Scripts/ImageProcessing.cs
@@ -1,95 +1,137 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using OpenCvSharp;
-using UnityEngine.UI;
+﻿// --------------------------------------------------------------------------
+//------- Desarrolladores: -----------------------------
+//-------- David Andrés Torres Betancour-------------------------------------------
+//-------  Contacto : davida.torres@udea.edu.co --------------
+//-------  Jenny Carolina Escobar Sozas    -----------------
+//-------  Contacto:    carolina.escobar@udea.edu.co -------------------
+//------- Proyecto 'Banana Gun' del Curso Procesamiento Digital de Imagenes----
+//------- V1.5 Abril de 2011--------------------------------------------------
+//--------------------------------------------------------------------------
+
+using UnityEngine;// Importación de la libreria principal de Unity
+using OpenCvSharp; // Importación de la Libreria de OpenCv
+using UnityEngine.UI; // Importación de la libreria de interfaz de usuario de Unity
+
+/// <summary>
+/// Clase que se encarga de controlar la camara y tomar cada snapshot que sera procesado
+/// por la clase ObjectDetection
+/// </summary>
+/// 
 public class ImageProcessing : MonoBehaviour
 {
+    //Objeto que contiene el contenedor para mostrar la imagen de fondo
     public RawImage rawImageCamera;
-
+    //Tolerancia de movimiento de la bolita
     public int toleranceMuzzle;
-
-    public int thresholdLazer;
-
+    //Suma minima que debe tener la matriz con el lazer para ser detectado
     int umbralSum = 50000;
 
-   public bool findMuzzle;
+    //Objetos que se desean detectar
+    public bool findMuzzle;
     public  bool findLazer;
 
+    //Instancia de la clase pointer para obtener la posicion a la que esta apuntando
     Pointer pointer;
+    //Textura de la camara
     WebCamTexture webCamTexture;
 
-    private void Awake()
-    {
-       
-    }
 
+    /// <summary>
+    /// Metodo que se corre por default de Unity al iniciar el juego
+    /// </summary>
     private void Start()
     {
-       
-            pointer = Pointer.instance;
+       //Se obtiene el Singlenton de la clase pointer
+        pointer = Pointer.instance;
 
+        //Si se va a jugar con el mouse 
         if (UIControls.mouseController)
         {           
+            //Se desactiva este objeto
             gameObject.SetActive(false);
            
         }
         else
         {
+            //Obtiene la camara principal del computador
             OpenCvTools.SetCameraTexture(ref webCamTexture);
-
+            //Se asigna la textura de la camara al contenedor que la va a mostrar
             rawImageCamera.texture = webCamTexture;
          
+            //Subscripción a las acciones para detener la camara
             GameEvent.instance.OnRoundEnds += StopCamera;
             GameEvent.instance.OnLoadingNextScene += StopCamera;
         }
     }
-
+    /// <summary>
+    /// Metodo por default de Unity que se ejecuta cuando el juego termina o se recarga la escena
+    /// </summary>
     private void OnDestroy()
     {
+        //Se detiene la cámara
         StopCamera();
+        //Se cancela la suscripción al evento
         GameEvent.instance.OnRoundEnds -= StopCamera;
     }
-
+    /// <summary>
+    /// Metodo por default de Unity que se ejecuta cuando el juego termina
+    /// </summary>
     private void OnApplicationQuit()
     {
         StopCamera();
     }
 
+    /// <summary>
+    /// Metodo por default de Unity que se ejecuta cada frame
+    /// </summary>
+
     private void Update()
     {
+        //Si la camara está encendida
         if (webCamTexture.isPlaying)
         {
+           //Se toma el snapshot de la camara y se obtiene la matriz de la imagen
             Mat mat = OpenCvSharp.Unity.TextureToMat(webCamTexture);
+            //Se convierte al espacio de colores Lab
             Cv2.CvtColor(mat, mat, ColorConversionCodes.BGR2Lab);
 
+            //Si esta habilitada la opcion de encontrar la bolita
             if (findMuzzle)
             {
-                ObjectDetection.SetMuzzlePosition(ref rawImageCamera, mat.ExtractChannel(2).Flip(FlipMode.Y),
-                                                toleranceMuzzle,
-                                                (int)pointer.rect.anchoredPosition.x,
-                                               -(int)pointer.rect.anchoredPosition.y);
+                // Se detecta y se obtiene ( si existe ) la posición del obeto
+                //Se manda solo el canal 3, es decir (b)  ya que es el canal donde se puede notar más
+                ObjectDetection.SetMuzzlePosition(mat.ExtractChannel(2).Flip(FlipMode.Y),
+                                                toleranceMuzzle, 
+                                                (int)pointer.rect.anchoredPosition.x, // Posicion del objeto con lo que se apunta en el eje X
+                                               -(int)pointer.rect.anchoredPosition.y); // Posicion del objeto con lo que se apunta en el eje Y
+
 
             }
+            //Si esta habilitada la opcion de encontrar el led
             if (findLazer)
             {
-                if (ObjectDetection.DetectLazer(ref rawImageCamera, thresholdLazer, mat.ExtractChannel(1).Flip(FlipMode.Y), umbralSum))
+                //Se detecta si el lazer existe o no
+                //Se manda el canal 2, es decir (a) ya que es el canal donde se puede notar mas el led
+                if (ObjectDetection.DetectLazer( mat.ExtractChannel(1).Flip(FlipMode.Y), umbralSum))
                 {
-                    
+                    //Si detecta el lazer empieza a disparar en la posición que se esta apuntando
                     GameEvent.instance.StartShoot(new Vector2(pointer.rect.anchoredPosition.x, 720 + pointer.rect.anchoredPosition.y));
                 }
             }
-
-
          
         }
     }
-
+    /// <summary>
+    /// Detiene la camara
+    /// </summary>
     void StopCamera()
     {
+        //Si la camara no existe no la detiene
         if (webCamTexture is null) return;
+        //Si la camara esta encendida
         if (webCamTexture.isPlaying)
         {
+            //Se detiene la camara
             webCamTexture.Stop();
         }
     }
--- Assets/Scripts/ObjectDetection.cs
+++ Assets/Scripts/ObjectDetection.cs
@@ -1,106 +1,170 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using OpenCvSharp;
+﻿// --------------------------------------------------------------------------
+//------- Desarrolladores: -----------------------------
+//-------- David Andrés Torres Betancour-------------------------------------------
+//-------  Contacto : davida.torres@udea.edu.co --------------
+//-------  Jenny Carolina Escobar Sozas    -----------------
+//-------  Contacto:    carolina.escobar@udea.edu.co -------------------
+//------- Proyecto 'Banana Gun' del Curso Procesamiento Digital de Imagenes----
+//------- V1.5 Abril de 2011--------------------------------------------------
+//--------------------------------------------------------------------------
+using UnityEngine;  // Importación de la libreria principal de Unity
+using OpenCvSharp; // Importación de la Libreria de OpenCv
+
+/// <summary>
+/// Esta clase contiene los metodos para detectar  el led y la bolita
+/// </summary>
 public class ObjectDetection 
 {
-   
-    public static void SetMuzzlePosition(ref RawImage image,Mat m, int tolerance,int lastXPixel, int lastYPixel)
+   /// <summary>
+   /// Este metodo se encarga de encontrar la posición de la bolita
+   /// </summary>
+   /// <param name="m">Matriz con los pixeles de la imagen </param>
+   /// <param name="tolerance"> Tolerancia de movimiento</param>
+   /// <param name="lastXPixel"> Última posición encontrada en el eje X</param>
+   /// <param name="lastYPixel"> Ultima posición encontrada en el eje Y</param>
+    public static void SetMuzzlePosition(Mat m, int tolerance,int lastXPixel, int lastYPixel)
     {
 
-
+        // Se crea matrices vacias que van a contener las proyecciones
         Mat col_proyection = new Mat();
         Mat row_proyection = new Mat();
 
+        //Se aplica umbralización binaria inversa, con un umbral de 102
         Cv2.Threshold(m, m, 102, 255, ThresholdTypes.BinaryInv);
 
+        // Se aplica Morfologia
+        ApplyMorphologyBall(ref m);
 
-        ApplyMorphology(ref m);
+       
 
-        //
-
-        // Proyection Vertical
+        // Se obtiene la  Proyection Vertical
         Cv2.Reduce(m, col_proyection, ReduceDimension.Column, ReduceTypes.Sum, MatType.CV_32F);
-        // Proyection Horizontal
+        // Se obtiene la  Proyection Horizontal
         Cv2.Reduce(m, row_proyection, ReduceDimension.Row, ReduceTypes.Sum, MatType.CV_32F);
 
+        // Si la suma de los elementos de la proyección esta vacia retorna ya que el elemento no existe
         if (Cv2.CountNonZero(col_proyection)<10 || Cv2.CountNonZero(row_proyection)<10)
         {
+            //Se le avisa a todos los elementos que estan suscritos a esta acción de que el elemento no fue detectado
             GameEvent.instance.MuzzelNotDetected();
             return;
         }
 
-        // Mean Proyection Vertical
+        // Se calcula la media de la  Proyection Vertical y Horizontal
         double col_mean = (Cv2.Sum(col_proyection).Val0) / Cv2.CountNonZero(col_proyection);
-        //Mean  Proyection Horizontal
         double row_mean = (Cv2.Sum(row_proyection).Val0) / Cv2.CountNonZero(row_proyection);
 
+        // Condición para asegurar que la media del objeto es lo suficientemente grande 
         bool objectExist = col_mean > 1000 && row_mean > 1000;
 
-       // Debug.Log($"Colmean: {col_mean} |  RowMean: {row_mean} | LastPixelX: {lastXPixel} | LastPixelX: {lastYPixel}");
-
+    
         if (objectExist)
         {
-           
+            //Si el objeto existe se procede a encontrar la posicion del objeto y luego se notifica a todos los objetos suscritos a esta acción de que la bolita fue detectada en esa posición
             GameEvent.instance.MuzzelDetected(GetObjectPosition(col_proyection, row_proyection, col_mean, row_mean, tolerance,lastXPixel, lastYPixel));
         }
      
     }
 
-    public static bool DetectLazer(ref RawImage rawImage, int threshHoldLazer,Mat m, int umbralSum)
+    /// <summary>
+    /// Este metodo se encarga de detectar el led
+    /// </summary>
+    /// <param name="m"> Matriz con los pixeles de la imagen </param>
+    /// <param name="umbralSum"> Sumatoria minima que debe tener el elemento para detectarlo</param>
+    /// <returns></returns>
+
+    public static bool DetectLazer(Mat m, int umbralSum)
     {
+        //Se aplica umbralización binaria con un umbral de 145
+        Cv2.Threshold(m, m, 145, 255, ThresholdTypes.Binary);
 
+        //Se aplica morfologia para eliminar el ruido
+        ApplyMorphologyLed(ref m);
 
-        Cv2.Threshold(m, m, threshHoldLazer, 255, ThresholdTypes.Binary);
-
-
-        Mat strElem = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
-        Cv2.MorphologyEx(m, m, MorphTypes.Open, strElem, iterations: 5);
-        // Cv2.MorphologyEx(m, m, MorphTypes.Close, strElem, iterations: 10);
-        Cv2.Dilate(m, m, strElem, iterations: 30);
-
-
-        return (Cv2.Sum(m).Val0 > umbralSum) ? true : false;
-
-        
-          
-
-        
+        // Si la sumatoria de los elementos de la matriz es mayor al umbral minimo quiere decir que el objeto existe
+        return (Cv2.Sum(m).Val0 > umbralSum) ? true : false;   
     }
-    static void ApplyMorphology(ref Mat m)
+    /// <summary>
+    /// Aplica morfologia a la matriz con la bolita
+    /// </summary>
+    /// <param name="m">Matriz con los pixeles de la imagen</param>
+    static void ApplyMorphologyBall(ref Mat m)
     {
+       // Se obtiene el elemento estructural que tiene forma de rectangulo con forma de 3x3 pixeles
         Mat strElem = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
 
+        // Se aplica Open para quitar pequeños punticos en la imagen
         Cv2.MorphologyEx(m, m, MorphTypes.Open, strElem, iterations: 3);
     }
+    /// <summary>
+    /// / Aplica morfologia a la matriz con la bolita
+    /// </summary>
+    /// <param name="m"> Matriz con los pixeles de la imagen</param>
+    static void ApplyMorphologyLed(ref Mat m)
+    {
+        Mat strElem = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
+        Cv2.MorphologyEx(m, m, MorphTypes.Open, strElem, iterations: 5);
+        Cv2.Dilate(m, m, strElem, iterations: 30);
+    }
 
+    /// <summary>
+    /// Obtiene la posición en el eje X y Y del objeto detectado
+    /// </summary>
+    /// <param name="col_proyection"> Matriz con la proyeccion vertical</param>
+    /// <param name="row_proyection">Matriz con la proyecction horizontal</param>
+    /// <param name="col_mean">Media de la proyeccion vertical</param>
+    /// <param name="row_mean">Media de la proyección horizontal</param>
+    /// <param name="tolerance">Tolerancia de movimiento</param>
+    /// <param name="lastXPixel"> Última posición encontrada en el eje X</param>
+    /// <param name="lastYPixel"> Ultima posición encontrada en el eje Y</param>
+    /// <returns>Retorna la posición del Objeto detectado</returns>
     static Vector2 GetObjectPosition(Mat col_proyection, Mat row_proyection, double col_mean, double row_mean, int tolerance, int lastX,int lastY)
     { 
-       return new Vector2(GetXAxis(row_proyection, row_mean, row_mean / 10, tolerance, lastX),
-                           GetYAxis(col_proyection, col_mean, col_mean / 10, tolerance,lastY));
+       return new Vector2(GetXAxis(row_proyection, row_mean, row_mean / 10, tolerance, lastX), //Obtiene posición en el eje X
+                           GetYAxis(col_proyection, col_mean, col_mean / 10, tolerance,lastY)); //Obtiene posición en e eje Y 
      }
 
+    /// <summary>
+    /// Obtiene la posición del objeto en el eje Y
+    /// </summary>
+    /// <param name="col_proyection">Matriz con la proyección vertical</param>
+    /// <param name="col_mean">Media de la matriz con la proyeccion vertical</param>
+    /// <param name="col_tolerance">Tolerancia de la media  en el eje Y</param>
+    /// <param name="tolerance"> Tolerancia de la emdia en el eje Y</param>
+    /// <param name="lastY">Ultima posición encontrada en el eje Y </param>
+    /// <returns>EL valor invertido de la posicion en el eje Y</returns>
     static float GetYAxis(Mat col_proyection, double col_mean, double col_tolerance, int tolerance, int lastY)
     {
+        //Se itera por cada fila de la matriz
         for (int i = 0; i < col_proyection.Rows; i++)
         {
+            //Se obtiene el valor
             float sum = col_proyection.At<float>(new int[] { i, 0 });
 
+            //Si la posición esta cerca a la media += cierta tolerancia significa que nos estamos acercando al centro del objeto
             if (sum > (col_mean - col_tolerance) && sum < (col_mean + col_tolerance))
             {
-                if (i >= lastY + tolerance || i <= lastY - tolerance) // Tolerance => 50 or -50 ( Standard desviation)
-                {
-                   
+                //Si la posición actual esta cerca a la posición anterior += la tolerancia se debe actualizar la posicion
+                if (i >= lastY + tolerance || i <= lastY - tolerance) 
+                {   
+                    //Se retorna el valor invertido de la posición en el eje Y
                     return -1 * i;
                 }
 
             }
         }
 
-        return -1*lastY;
+        return -1*lastY; // Si no sufrió un movimiento sustancial sigue retornando la posicion anterior
     }
-
+    /// <summary>
+    /// Obtiene la posición en el eje X del objeto
+    /// </summary>
+    /// <param name="row_proyection">Matriz con la proyección horizontal</param>
+    /// <param name="row_mean">Media de la matriz con la proyeccion horizontal</param>
+    /// <param name="row_tolerance">Tolerancia de la media  en el eje X</param>
+    /// <param name="tolerance">Tolerancia de la emdia en el eje X </param>
+    /// <param name="lastX"> Ultima posición encontrada en el eje X  </param>
+    /// <returns> Retorna la posición en el eje X</returns>
     static float GetXAxis(Mat row_proyection, double row_mean, double row_tolerance, int tolerance,int lastX)
     {
         for (int i = 0; i < row_proyection.Cols; i++)
@@ -118,6 +182,6 @@ public class ObjectDetection
             }
         }
 
-        return lastX;
+        return lastX; // Si no sufrió un movimiento sustancial sigue retornando la posicion anterior
     }
 }
